b67ab61c56004932b1f88b8a145a10e6111463d7
kryptoTomo <tomasz.madej101@gmail.com>
2022-01-13 10:18:41 +0100

    Add a test to read a State forwarded attribute

--- cpp_test_suite/cpp_test_ds/fwd_ds/FwdTestClass.h
+++ cpp_test_suite/cpp_test_ds/fwd_ds/FwdTestClass.h
@@ -81,6 +81,13 @@ public:
 	~FwdAttrImaStr() {};
 };
 
+class FwdAttrScaSta: public Tango::FwdAttr
+{
+public:
+	FwdAttrScaSta(const std::string &_n):FwdAttr(_n) {};
+	~FwdAttrScaSta() {};
+};
+
 /*----- PROTECTED REGION END -----*/	//	FwdTestClass::classes for dynamic creation
 
 /**
