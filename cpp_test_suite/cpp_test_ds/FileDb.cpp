9093670055fd2cb1b6f52793a3bfbe98ece75385
Thomas Braun <thomas.braun@byte-physics.de>
2022-05-10 16:32:51 +0200

    cpp_test_suite/cpp_test_ds: Namespace qualify cout

--- cpp_test_suite/cpp_test_ds/FileDb.cpp
+++ cpp_test_suite/cpp_test_ds/FileDb.cpp
@@ -12,7 +12,7 @@
 
 void DevTest::FileDb()
 {
-    	cout << "[DevTest::FileDb] received" << std::endl;
+    	std::cout << "[DevTest::FileDb] received" << std::endl;
 
 //
 // First DbInfo call
@@ -21,14 +21,14 @@ void DevTest::FileDb()
 	Tango::Util *tg = Tango::Util::instance();
 	Tango::Database *db = tg->get_database();
 	std::string db_inf = db->get_info();
-	cout << db_inf << std::endl;
+	std::cout << db_inf << std::endl;
 
 //
 // Test class property
 //
 
 	Tango::DbClass db_class("DevTest",db);
-	cout << "   DbClass object created --> OK" << std::endl;
+	std::cout << "   DbClass object created --> OK" << std::endl;
 
 	Tango::DbData db_dat_put,db_dat_get,db_dat_del;
 	Tango::DbDatum cl_prop("ClassTest");
@@ -36,14 +36,14 @@ void DevTest::FileDb()
 	db_class.get_property(db_dat_get);
 
 	assert (db_dat_get[0].is_empty() == true);
-	cout << "   Property not defined --> OK" << std::endl;
+	std::cout << "   Property not defined --> OK" << std::endl;
 	db_dat_get.clear();
 
 	cl_prop << "Hello girl";
 	db_dat_put.push_back(cl_prop);
 
 	db_class.put_property(db_dat_put);
-	cout << "   Put class property --> OK" << std::endl;
+	std::cout << "   Put class property --> OK" << std::endl;
 
 	Tango::DbDatum cl_prop_get("ClassTest");
 	db_dat_get.push_back(cl_prop_get);
@@ -53,7 +53,7 @@ void DevTest::FileDb()
 	std::string pr;
 	db_dat_get[0] >> pr;
 	assert(pr == "Hello girl");
-	cout << "   Get class property --> OK" << std::endl;
+	std::cout << "   Get class property --> OK" << std::endl;
 	db_dat_get.clear();
 
 	Tango::DbDatum cl_prop_del("ClassTest");
@@ -65,7 +65,7 @@ void DevTest::FileDb()
 	db_class.get_property(db_dat_get);
 	assert (db_dat_get[0].is_empty() == true);
 
-	cout << "   Delete class property --> OK" << std::endl;
+	std::cout << "   Delete class property --> OK" << std::endl;
 
 //
 // Test device property
@@ -79,15 +79,15 @@ void DevTest::FileDb()
 	db_d->get_property(dev_dat_get);
 
 	assert (dev_dat_get[0].is_empty() == true);
-	cout << std::endl;
-	cout << "   Device property not defined --> OK" << std::endl;
+	std::cout << std::endl;
+	std::cout << "   Device property not defined --> OK" << std::endl;
 	dev_dat_get.clear();
 
 	dev_prop << "Hello women";
 	dev_dat_put.push_back(dev_prop);
 
 	db_d->put_property(dev_dat_put);
-	cout << "   Put device property --> OK" << std::endl;
+	std::cout << "   Put device property --> OK" << std::endl;
 
 	Tango::DbDatum dev_prop_get("DeviceTest");
 	dev_dat_get.push_back(dev_prop_get);
@@ -96,7 +96,7 @@ void DevTest::FileDb()
 	assert (dev_dat_get[0].is_empty() == false);
 	dev_dat_get[0] >> pr;
 	assert (pr == "Hello women");
-	cout << "   Get device property --> OK" << std::endl;
+	std::cout << "   Get device property --> OK" << std::endl;
 	dev_dat_get.clear();
 
 	Tango::DbDatum dev_prop_del("DeviceTest");
@@ -108,28 +108,28 @@ void DevTest::FileDb()
 	db_d->get_property(db_dat_get);
 	assert (dev_dat_get[0].is_empty() == true);
 
-	cout << "   Delete device property --> OK" << std::endl;
+	std::cout << "   Delete device property --> OK" << std::endl;
 
 
 //
 // Test class attribute property
 //
 
-	cout << std::endl;
+	std::cout << std::endl;
 	Tango::DbData cl_att_put,cl_att_get,cl_att_del;
 	Tango::DbDatum cl_att_prop("ClassTest");
 	cl_att_get.push_back(cl_att_prop);
 	db_class.get_property(cl_att_get);
 
 	assert (cl_att_get[0].is_empty() == true);
-	cout << "   Class attribute property not defined --> OK" << std::endl;
+	std::cout << "   Class attribute property not defined --> OK" << std::endl;
 	cl_att_get.clear();
 
 	cl_att_prop << "You look nice";
 	cl_att_put.push_back(cl_att_prop);
 
 	db_class.put_property(cl_att_put);
-	cout << "   Put class attribute property --> OK" << std::endl;
+	std::cout << "   Put class attribute property --> OK" << std::endl;
 
 	Tango::DbDatum cl_att_prop_get("ClassTest");
 	cl_att_get.push_back(cl_att_prop_get);
@@ -138,7 +138,7 @@ void DevTest::FileDb()
 	assert (cl_att_get[0].is_empty() == false);
 	cl_att_get[0] >> pr;
 	assert( pr == "You look nice");
-	cout << "   Get class attribute property --> OK" << std::endl;
+	std::cout << "   Get class attribute property --> OK" << std::endl;
 	cl_att_get.clear();
 
 	Tango::DbDatum cl_att_prop_del("ClassTest");
@@ -150,27 +150,27 @@ void DevTest::FileDb()
 	db_class.get_property(cl_att_get);
 	assert (cl_att_get[0].is_empty() == true);
 
-	cout << "   Delete class attribute property --> OK" << std::endl;
+	std::cout << "   Delete class attribute property --> OK" << std::endl;
 
 //
 // Test device attribute property
 //
 
-	cout << std::endl;
+	std::cout << std::endl;
 	Tango::DbData dev_att_put,dev_att_get,dev_att_del;
 	Tango::DbDatum dev_att_prop("DeviceTest");
 	dev_att_get.push_back(dev_att_prop);
 	db_d->get_property(dev_att_get);
 
 	assert (dev_att_get[0].is_empty() == true);
-	cout << "   Device attribute property not defined --> OK" << std::endl;
+	std::cout << "   Device attribute property not defined --> OK" << std::endl;
 	dev_att_get.clear();
 
 	dev_att_prop << "You look nice";
 	dev_att_put.push_back(cl_att_prop);
 
 	db_d->put_property(dev_att_put);
-	cout << "   Put device attribute property --> OK" << std::endl;
+	std::cout << "   Put device attribute property --> OK" << std::endl;
 
 	Tango::DbDatum dev_att_prop_get("ClassTest");
 	dev_att_get.push_back(dev_att_prop_get);
@@ -179,7 +179,7 @@ void DevTest::FileDb()
 	assert (dev_att_get[0].is_empty() == false);
 	dev_att_get[0] >> pr;
 	assert( pr == "You look nice");
-	cout << "   Get device attribute property --> OK" << std::endl;
+	std::cout << "   Get device attribute property --> OK" << std::endl;
 	dev_att_get.clear();
 
 	Tango::DbDatum dev_att_prop_del("ClassTest");
@@ -191,6 +191,6 @@ void DevTest::FileDb()
 	db_d->get_property(dev_att_get);
 	assert (dev_att_get[0].is_empty() == true);
 
-	cout << "   Delete device attribute property --> OK" << std::endl;
+	std::cout << "   Delete device attribute property --> OK" << std::endl;
 }
 
