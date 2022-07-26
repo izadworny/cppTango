9cb94b50c64391eace7fc2a6e9fdb3cfed4b5ca7
Thomas Braun <thomas.braun@byte-physics.de>
2022-07-26 13:07:10 +0200

    Tests/cpp_test_ds/main.cpp: Fix unclean server shutdown
    In the last commit a fix was added which refetches the Tango::Util object
    if it was not yet fetched.
    
    But that does not work as Tango::Util::instance returns the current
    instance which can also be null. In addition it also prints an error
    message in this case.
    
    As we only can do the server cleanup if we have an Util object, the fix is
    to not do the cleanup in all other cases.

--- tests/device_server/generic/main.cpp
+++ tests/device_server/generic/main.cpp
@@ -53,9 +53,11 @@ int main(int argc,char *argv[])
 		std::cout << "Exiting" << std::endl;
 	}
 
-//
-// Destroy the ORB (and properly releases its resources)
-//
-	Tango::Util::instance()->server_cleanup();
+  // Destroy the ORB (and properly release its resources)
+  if(tg)
+  {
+    tg->server_cleanup();
+  }
+
 	return(0);
 }
