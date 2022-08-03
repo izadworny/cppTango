izadworny/cppTango

commit 02940d9c15f5108ce39e6640ed148213128d443a

Add unit tests for jpeg encoding.
Test is configured via cmake to access the resources, hence the
modification in cxxtest/CMakelists.txt to find configured file, that is
in the build tree, and not the source one.

Modification of the run with fixtures script to allow not to use the
fixture (allow for this test to run much faster).

Image used is peppers from
http://sipi.usc.edu/database/database.php?volume=misc. The image is free
of right. Gimp was used to extract raw data from the input tiff file.

cpp_test_suite/CMakeLists.txt
@@ -13,6 +13,7 @@ set(FWD_DEV "${INST_NAME}/fwd_debian8/10")
 set(DEV1_ALIAS "debian8_alias")
 set(ATTR_ALIAS "debian8_attr_alias")
 set(COMMON_TEST_CXX_DEFS -DVALGRIND -D_PTHREADS -D_REENTRANT)
+set(RESOURCES_DIR "${CMAKE_CURRENT_SOURCE_DIR}/resources/")
 cmake_host_system_information(RESULT HOST_NAME QUERY FQDN)
 string(TOLOWER ${HOST_NAME} HOST_NAME)
 message("HOST_NAME=${HOST_NAME}")

cpp_test_suite/cxxtest/CMakeLists.txt
@@ -6,19 +6,34 @@ if(NOT PYTHON_INTERPRETER)
 endif()
 
 function(CXX_GENERATE_TEST name)
+
+    if(${ARGC} EQUAL 2)
+        if(ARGV1)
+            set(SKIP_FIXTURES "--skip-fixtures")
+        endif()
+    endif()
+
+    #In case of substitution to be done by cmake, the test file will be in the build tree
+    IF(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../new_tests/${name}.cpp")
+        set(INPUT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/../new_tests/${name}.cpp")
+    ELSE()
+        set(INPUT_FILE "${CMAKE_CURRENT_BINARY_DIR}/../new_tests/${name}.cpp")
+    ENDIF()
     add_custom_command(OUTPUT ${name}.cpp
                        COMMAND "${PYTHON_INTERPRETER}" ${CMAKE_CURRENT_SOURCE_DIR}/bin/cxxtestgen.py
                                --template=${CMAKE_CURRENT_SOURCE_DIR}/template/tango_template.tpl
                                -o ${name}.cpp
-                               ${CMAKE_CURRENT_SOURCE_DIR}/../new_tests/${name}.cpp
+                               ${INPUT_FILE}
                        DEPENDS tango
-                               ${CMAKE_CURRENT_SOURCE_DIR}/../new_tests/${name}.cpp
+                               ${INPUT_FILE}
                                ${CMAKE_CURRENT_SOURCE_DIR}/template/tango_template.tpl
                        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                        COMMENT "Generate ${name}.cpp")
 
     add_executable(${name} $<TARGET_OBJECTS:compare_test_object> ${name}.cpp)
     target_include_directories(${name} PRIVATE include ${CMAKE_CURRENT_BINARY_DIR}/include)
+    target_include_directories(${name} PRIVATE
+	    ${CMAKE_CURRENT_SOURCE_DIR}/../new_tests)
     target_link_libraries(${name} PRIVATE tango ${CMAKE_THREAD_LIBS_INIT})
     target_compile_definitions(${name} PRIVATE ${COMMON_TEST_CXX_DEFS})
     if(USE_PCH)
@@ -26,6 +41,7 @@ function(CXX_GENERATE_TEST name)
     endif()
 
     tango_add_test(NAME "CXX::${name}" COMMAND $<TARGET_FILE:${name}>
+            ${SKIP_FIXTURES}
             --device1=${DEV1}
             --device2=${DEV2}
             --device3=${DEV3}
@@ -88,5 +104,9 @@ CXX_GENERATE_TEST(cxx_reconnection_zmq)
 CXX_GENERATE_TEST(cxx_stateless_subscription)
 CXX_GENERATE_TEST(cxx_nan_inf_in_prop)
 CXX_GENERATE_TEST(cxx_asyn_reconnection)
+
+configure_file(../new_tests/cxx_jpeg_encoding.cpp.in
+	../new_tests/cxx_jpeg_encoding.cpp)
+CXX_GENERATE_TEST(cxx_jpeg_encoding TRUE)
 
 configure_file(include/config.h.cmake include/config.h @ONLY)

cpp_test_suite/environment/run_with_fixture.sh.cmake
@@ -22,6 +22,13 @@ tc_program="$1"
 tc_run_name="$(basename "${tc_program}")_$(date '+%Y%m%d.%H%M%S.%N')"
 shift 1
 
+if [ $# -gt 0 ]; then
+    case $1 in
+        --skip-fixtures) TANGO_TEST_CASE_SKIP_FIXTURE=1; shift 1;;
+        *) ;;
+    esac
+fi
+
 tc_mysql_container="mysql_db_${tc_run_name}"
 tc_tango_container="tango_cs_${tc_run_name}"
 

cpp_test_suite/new_tests/cxx_jpeg_encoding.cpp.in
@@ -1,0 +1,194 @@
+#ifndef JPEGEncodedTestSuite_h
+#define JPEGEncodedTestSuite_h
+
+#include <ctime>
+#include <cstdio>
+#include <iterator>
+#include <memory>
+#include <vector>
+#include <cxx_common.h>
+
+#undef SUITE_NAME
+#define SUITE_NAME JPEGEncodedTestSuite
+
+class JPEGEncodedTestSuite: public CxxTest::TestSuite
+{
+    protected:
+
+        std::unique_ptr<Tango::EncodedAttribute> encoder;
+
+        std::vector<unsigned char> raw_8bits;
+        std::vector<unsigned char> raw_24bits;
+        std::vector<unsigned char> raw_32bits;
+
+        std::vector<unsigned char> jpeg_rgb;
+        std::vector<unsigned char> jpeg_gray;
+
+        std::vector<unsigned char> decoded_rgb;
+        std::vector<unsigned char> decoded_gray;
+
+        std::size_t jpeg_rgb_start_offset;
+        std::size_t jpeg_gray_start_offset;
+
+        std::vector<unsigned char> load_file(const std::string& file)
+        {
+            std::ifstream read_file(file, std::ios::binary);
+            assert(read_file.is_open());
+
+            return std::vector<unsigned char>{
+                std::istreambuf_iterator<char>(read_file),
+                {}};
+        }
+
+        template <typename T>
+        std::size_t find_jpeg_start(const T* buffer, std::size_t length)
+        {
+            assert(length > 1);
+            for(std::size_t i = 0 ; i < length - 1; ++i)
+            {
+                if(buffer[i] == 0xFF && buffer[i+1] == 0xDA)
+                {
+                    return i;
+                }
+            }
+            assert(false);
+        }
+
+    public:
+        SUITE_NAME()
+        {
+
+            //
+            // Arguments check -------------------------------------------------
+            //
+
+            CxxTest::TangoPrinter::validate_args();
+
+
+            // Initialization --------------------------------------------------
+
+            // CMake substitution to set the resource directory
+            std::string resource_path = "@RESOURCES_DIR@";
+
+            // Load all the data needed for the test
+            raw_24bits = load_file(resource_path+"/peppers.data");
+            raw_32bits = load_file(resource_path+"/peppers_alpha.data");
+            raw_8bits = load_file(resource_path+"/peppers_gray.data");
+            decoded_gray = load_file(resource_path+"/peppers_gray_decoded.data");
+            decoded_rgb = load_file(resource_path+"/peppers_decoded.data");
+            jpeg_rgb = load_file(resource_path+"/peppers.jpeg");
+
+            // Set the position of the beginning of the jpeg image, not to compare header data.
+            jpeg_rgb_start_offset = find_jpeg_start(jpeg_rgb.data(), jpeg_rgb.size());
+
+            jpeg_gray = load_file(resource_path+"/peppers_gray.jpeg");
+
+            // Set the position of the beginning of the jpeg image, not to compare header data.
+            jpeg_gray_start_offset = find_jpeg_start(jpeg_gray.data(), jpeg_gray.size());
+
+            encoder = std::make_unique<Tango::EncodedAttribute>();
+        }
+
+        virtual ~SUITE_NAME()
+        {
+
+            //
+            // Clean up --------------------------------------------------------
+            //
+        }
+
+        static SUITE_NAME *createSuite()
+        {
+            return new SUITE_NAME();
+        }
+
+        static void destroySuite(SUITE_NAME *suite)
+        {
+            delete suite;
+        }
+
+        //
+        // Tests -------------------------------------------------------
+        //
+        // Check the encoding functions
+        void test_jpeg_encoding()
+        {
+            encoder->encode_jpeg_gray8(raw_8bits.data(), 512, 512, 100);
+            std::size_t offset = find_jpeg_start(encoder->get_data(), encoder->get_size());
+
+            TS_ASSERT_SAME_DATA(jpeg_gray.data()+jpeg_gray_start_offset, encoder->get_data()+offset, 1000);
+
+            encoder->encode_jpeg_rgb24(raw_24bits.data(), 512, 512, 100);
+
+            offset = find_jpeg_start(encoder->get_data(), encoder->get_size());
+            TS_ASSERT_SAME_DATA(jpeg_rgb.data()+jpeg_rgb_start_offset, encoder->get_data()+offset, 1000);
+
+            encoder->encode_jpeg_rgb32(raw_32bits.data(), 512, 512, 100);
+
+            offset = find_jpeg_start(encoder->get_data(), encoder->get_size());
+            TS_ASSERT_SAME_DATA(jpeg_rgb.data()+jpeg_rgb_start_offset, encoder->get_data()+offset, 1000);
+
+            // Check if it throws errors
+            TS_ASSERT_THROWS_ASSERT(encoder->encode_jpeg_gray8(raw_8bits.data(), 0, 0, 100), Tango::DevFailed &e,
+                    TS_ASSERT_EQUALS(std::string(e.errors[0].reason.in()), "API_EncodeErr"));
+            TS_ASSERT_THROWS_ASSERT(encoder->encode_jpeg_rgb24(raw_8bits.data(), 0, 0, 100), Tango::DevFailed &e,
+                    TS_ASSERT_EQUALS(std::string(e.errors[0].reason.in()), "API_EncodeErr"));
+            TS_ASSERT_THROWS_ASSERT(encoder->encode_jpeg_rgb32(raw_8bits.data(), 0, 0, 100), Tango::DevFailed &e,
+                    TS_ASSERT_EQUALS(std::string(e.errors[0].reason.in()), "API_EncodeErr"));
+        }
+
+        // Check the decoding functions
+        void test_jpeg_decoding()
+        {
+            DeviceAttribute da_rgb, da_gray, da_error;
+            Tango::DevEncoded att_de_rgb, att_de_gray, att_de_error;
+            int width, height;
+            unsigned char* color_buffer = nullptr;
+            unsigned char* gray_buffer = nullptr;
+
+            att_de_rgb.encoded_format = "JPEG_RGB";
+            att_de_rgb.encoded_data.length(jpeg_rgb.size());
+            Tango::DevVarCharArray data_rgb(jpeg_rgb.size(), jpeg_rgb.size(), jpeg_rgb.data(), false);
+            att_de_rgb.encoded_data = data_rgb;
+
+            da_rgb << att_de_rgb;
+
+            att_de_gray.encoded_format = "JPEG_GRAY8";
+            Tango::DevVarCharArray data_gray(jpeg_gray.size(), jpeg_gray.size(), jpeg_gray.data(), false);
+            att_de_gray.encoded_data = data_gray;
+
+            da_gray << att_de_gray;
+
+            att_de_error.encoded_format = "JPEG_GRAY8";
+            Tango::DevVarCharArray data_gray_error(raw_8bits.size(), raw_8bits.size(), raw_8bits.data(), false);
+            att_de_error.encoded_data = data_gray_error;
+
+            da_error << att_de_error;
+
+            // Decode jpeg images
+            encoder->decode_rgb32(&da_rgb, &width, &height, &color_buffer);
+
+            TS_ASSERT_EQUALS(width, 512);
+            TS_ASSERT_EQUALS(height, 512);
+
+            TS_ASSERT_SAME_DATA(decoded_rgb.data(), &color_buffer[0], 1000);
+
+            encoder->decode_gray8(&da_gray, &width, &height, &gray_buffer);
+            TS_ASSERT_EQUALS(width, 512);
+            TS_ASSERT_EQUALS(height, 512);
+
+            TS_ASSERT_SAME_DATA(decoded_gray.data(), &gray_buffer[0], 1000);
+
+            // Check if it throws errors
+            TS_ASSERT_THROWS_ASSERT(encoder->decode_gray8(&da_error, &width, &height, &color_buffer), Tango::DevFailed &e,
+                    TS_ASSERT_EQUALS(string(e.errors[0].reason.in()), "API_DecodeErr"));
+            TS_ASSERT_THROWS_ASSERT(encoder->decode_rgb32(&da_error, &width, &height, &color_buffer), Tango::DevFailed &e,
+                    TS_ASSERT_EQUALS(string(e.errors[0].reason.in()), "API_WrongFormat"));
+
+            delete[] color_buffer;
+            delete[] gray_buffer;
+
+        }
+};
+#undef cout
+#endif // JPEGEncodedTestSuite_h

commit 846fd928755310f8fe3c7c050985ceb9457ab15c

[ubsan] cxx_jpeg_encoding.cpp.in: Avoid implicit-integer-sign-change UBSAN error
Bug introduced in 02940d9c (Add unit tests for jpeg encoding.,
2021-09-22).

tests/cxx_jpeg_encoding.cpp
@@ -42,9 +42,18 @@ class JPEGEncodedTestSuite: public CxxTest::TestSuite
             std::ifstream read_file(file, std::ios::binary);
             assert(read_file.is_open());
 
-            return std::vector<unsigned char>{
+            auto signed_vec = std::vector<char>{
                 std::istreambuf_iterator<char>(read_file),
                 {}};
+
+
+            // workaround UBSAN integer-implicit-sign-change error
+            const size_t length = signed_vec.size();
+
+            std::vector<unsigned char> unsigned_vec(length);
+            memcpy(unsigned_vec.data(), reinterpret_cast<unsigned char*>(signed_vec.data()), length);
+
+            return unsigned_vec;
         }
 
         template <typename T>