64d43ce1139a4da77555354e68d68980d68ce11d
Thomas Juerges <thomas.juerges@skao.int>
2022-08-05 13:17:46 +0100

    Add __attribute__((unused)) to unused variables for non-MSVC compilers

--- configure/test_cppzmq_features.cpp
+++ configure/test_cppzmq_features.cpp
@@ -1,20 +1,27 @@
 #include <zmq.hpp>
 
+#ifdef _TG_WINDOWS_
+    #define __IS_UNUSED__(var) var
+#else
+    #define __IS_UNUSED__(var) var __attribute__((unused))
+#endif
+
 int main(int, char**)
 {
   zmq::context_t c(1);
   zmq::socket_t s(c, ZMQ_REQ);
-  void* plain_socket = s.handle();
+  void* __IS_UNUSED__(plain_socket) = s.handle();
 
   s.disconnect("some endpoint");
 
   zmq::message_t m, n;
-  zmq::send_result_t sr = s.send(m, zmq::send_flags::none);
-  zmq::recv_result_t rr = s.recv(m, zmq::recv_flags::none);
+  zmq::send_result_t __IS_UNUSED__(sr)= s.send(m, zmq::send_flags::none);
+  zmq::recv_result_t __IS_UNUSED__(rr) = s.recv(m, zmq::recv_flags::none);
 
   m.copy(n);
   m.move(n);
 
   s.set(zmq::sockopt::linger, 0);
-  int val = s.get(zmq::sockopt::linger);
+  int __IS_UNUSED__(val) = s.get(zmq::sockopt::linger);
+  return 0;
 }
